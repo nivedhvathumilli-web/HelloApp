66cd70502d7928be48bd518e070963261d67a156
Nivedh <nivedh.vathumilli@gmail.com>
2026-02-17 09:05:24 +0530

    feature/UC1-PrintOops

---
+++ OopsBanner/Nivedh_java.java
@@ -0,0 +1,12 @@
+public class Nivedh_java {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        // Sample variable
+        String name = "Nivedh";
+        int age = 25;
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
